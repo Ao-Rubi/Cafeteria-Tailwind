c9851aac6b585c00e2f814611021f103cf6cc657
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2023-03-03 18:49:38 -0300

    Map de los productos

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CardProducto from './products/CardProducto';
 
 const Home = () => {
+
+    const URL = process.env.REACT_APP_API_SERVER;
+
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+        consultarAPI();
+    }, []);
+
+    const consultarAPI = async()=>{
+
+        //Peticion Get
+        try {
+            const resultado = await fetch(URL)
+            const listarProductos = await resultado.json();
+
+            setProductos(listarProductos)
+        } catch (error) {
+            console.error(error)
+        }
+
+    }
+
     return (
-        <div>
-            Home page
+        <div className='px-5'>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 py-5">
+
+                {productos.map( (producto)=>{return <CardProducto key={producto._id} producto={producto} ></CardProducto>} )}
+
+            </div>
         </div>
+
     );
 };
 
